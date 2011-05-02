5dd47e70dddd8d4fa3ee512e08fed1013c766417
Leif Wickland <leifwickland@gmail.com>
2011-05-02 10:36:48 -0600

    Ramped it up and tried a different approach to avoid running so many threads.

--- pipeline/sleepyPipeline.scala
+++ pipeline/sleepyPipeline.scala
@@ -5,17 +5,29 @@ import java.util.Random
 
 object sleepyPipeline {
   def main(args : Array[String]) {
-    println("Main starting")
+    if (args.length != 3) {
+      println("Usage: middles rights sleepMillis")
+      return
+    }
+    val middleCount = args(0).toInt
+    val rightCount = args(1).toInt
+    val sleepMillis = args(2).toInt
+    println("Main starting: " + middleCount + " " + rightCount + " " + sleepMillis)
     val left = new Left
     left.start
-    for (i <- 1 to 10) {
+    
+    val rights = new scala.collection.mutable.ListBuffer[Right]
+    for (i <- 1 to middleCount) {
       var middle = new Middle(left, i.toString)
       middle.start
-      for (j <- 1 to 10) { 
-        val right = new Right(middle, (j + (i-1) * 4).toString)
+      for (j <- 1 to rightCount) { 
+        val right = new Right(middle, (j + (i-1) * rightCount).toString, sleepMillis)
         right.start
+        //rights.append(right)
       }
     }
+
+    //rights.foreach(right => right.start)
     println("Main exiting")
   }
 }
@@ -33,15 +45,14 @@ class Left extends Actor {
     listenerCount -= 1
     printf("Somebody left! (now %d)\n", listenerCount)
     if (listenerCount <= 0) {
-      println("No more listeners")
+      println("No more listeners. Exiting with i = " + i)
       exit("no more litsteners")
     }
   }
 
   def act {
-    println("Top Left act")
     loop {
-      react {
+      receive {
         case Next => i += 1; reply(i)
         case Join => join
         case Leave => leave
@@ -53,19 +64,17 @@ class Left extends Actor {
 class Middle(left: Left, name: String) extends Actor {
   var listenerCount = 0
   override def act {
-    println("Top Middle act")
-    val rand = new Random
     left ! Join
     loop {
-      react {
-        case Next => Thread.sleep(rand.nextInt(100)); reply(left !? Next)
+      receive {
+        case Next => reply(left !? Next)
         case Join => {
           listenerCount += 1
-          printf("Middle %s got a new listener and has %d listeners\n", name, listenerCount)
+          //printf("Middle %s got a new listener and has %d listeners\n", name, listenerCount)
         }
         case Leave => {
-          listenerCount += -1
-          printf("Middle %s lost a listener and has %d listeners\n", name, listenerCount)
+          listenerCount -= 1
+          //printf("Middle %s lost a listener and has %d listeners\n", name, listenerCount)
           if (listenerCount <= 0) {
             left ! Leave
             exit("no more litsteners")
@@ -76,18 +85,41 @@ class Middle(left: Left, name: String) extends Actor {
   }
 }
 
-class Right(middle: Middle, name: String) extends Actor {
+class Right(middle: Middle, name: String, sleepMillis: java.lang.Integer) extends Actor {
+  val rand = new Random
   override def act {
-    println("Top Right act")
-    printf("Right %s starting up!", name);
     middle ! Join
+    printf("Right %s is starting\n", name);
+    var i = 0
+    this ! Next
+    loop {
+      react {
+        case Next => {
+          i += 1
+          if (sleepMillis > 0) Thread.sleep(rand.nextInt(sleepMillis));
+          var received = middle !? Next
+          printf("Right %s at %d got %d\n", name, i, received)
+          if (i < 100) {
+            this ! Next
+          }
+          else {
+            middle ! Leave
+            printf("Right %s is spent, but last got %d!\n", name, received);
+            exit
+          }
+        }
+      }
+    }
+    /*
     for (i <- 1 to 100) { 
       printf("Right %s at %d got %d\n", name, i, middle !? Next)
     }
     middle ! Leave
     printf("Right %s is spent!", name);
+    */
   }
 }
+
 case class Next()
 case class Join()
 case class Leave()
